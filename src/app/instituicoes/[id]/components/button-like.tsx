4cc4edd23779b2502139f66c720816f8ca044325
gabriel-ac6 <95120872+gabriel-ac6@users.noreply.github.com>
2024-06-27 16:31:24 -0300

    Update button-like.tsx

--- src/app/instituicoes/[id]/components/button-like.tsx
+++ src/app/instituicoes/[id]/components/button-like.tsx
@@ -2,25 +2,44 @@
 
 import { Heart } from '@phosphor-icons/react/dist/ssr'
 import { useState } from 'react'
+import { useMutation } from '@tanstack/react-query'
 
 import { Button } from '@/components/ui/button'
 import { cn } from '@/lib/utils'
+import { serviceUpdateReactType } from '../../../../services/institution/like'
 
-export function ButtonLike() {
+interface ButtonLikeProps {
+  reviewId: number
+}
+
+export function ButtonLike({ reviewId }: ButtonLikeProps) {
   const [liked, setLiked] = useState(false)
 
+  const mutation = useMutation({
+    mutationFn: (params: { reviewId: number; reactType: number }) =>
+      serviceUpdateReactType(params),
+    onSuccess: () => {
+      console.log('React type updated successfully')
+    },
+    onError: (error: Error) => {
+      console.error('Error updating react type:', error.message)
+    },
+  })
+
+  const handleClick = () => {
+    const newLiked = !liked
+    setLiked(newLiked)
+    mutation.mutate({ reviewId, reactType: newLiked ? 1 : 0 }) // Passando reviewId e reactType
+  }
+
   return (
-    <Button
-      variant={'outline'}
-      size={'xs'}
-      onClick={() => setLiked((oldLiked) => !oldLiked)}
-    >
+    <Button variant={'outline'} size={'xs'} onClick={handleClick}>
       <Heart
         weight={liked ? 'fill' : 'bold'}
         size={16}
         className={cn(
           'transition-all duration-200',
-          liked && 'text-danger-700 ',
+          liked && 'text-danger-700',
         )}
       />
     </Button>
