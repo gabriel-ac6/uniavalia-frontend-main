d00c9c9957cec087612f2387a576742d9a3d07da
gabriel-ac6 <95120872+gabriel-ac6@users.noreply.github.com>
2024-06-27 17:27:58 -0300

    Update button-links.tsx

--- src/components/layout/navbar/button-links.tsx
+++ src/components/layout/navbar/button-links.tsx
@@ -1,20 +1,22 @@
-import { ArrowRight } from '@phosphor-icons/react/dist/ssr'
-import Link from 'next/link'
-import { ReactNode } from 'react'
+'use client'
+import { ArrowRight } from '@phosphor-icons/react/dist/ssr';
+import Link from 'next/link';
+import { ReactNode, useEffect, useState } from 'react';
+import { getSession } from 'next-auth/react';
 
-import { Button } from '@/components/ui/button'
-import { SheetTrigger } from '@/components/ui/sheet'
+import { Button } from '@/components/ui/button';
+import { SheetTrigger } from '@/components/ui/sheet';
 
 interface ButtonLinksProps {
-  isMobile?: boolean
+  isMobile?: boolean;
 }
 
 interface ButtonLinkProps extends ButtonLinksProps {
-  children?: ReactNode
+  children?: ReactNode;
 }
 
 function LinkArrow() {
-  return <ArrowRight size={16} className="text-primary-700 lg:hidden" />
+  return <ArrowRight size={16} className="text-primary-700 lg:hidden" />;
 }
 
 function ButtonLink({ children, isMobile }: ButtonLinkProps) {
@@ -23,24 +25,42 @@ function ButtonLink({ children, isMobile }: ButtonLinkProps) {
       {children}
       <LinkArrow />
     </Button>
-  )
+  );
 
   if (isMobile) {
-    return <SheetTrigger>{ButtonElement}</SheetTrigger>
+    return <SheetTrigger>{ButtonElement}</SheetTrigger>;
   }
 
-  return ButtonElement
+  return ButtonElement;
 }
 
 export function ButtonLinks({ isMobile = false }: ButtonLinksProps) {
+  const [sessionLoading, setSessionLoading] = useState(true);
+  const [sessionActive, setSessionActive] = useState(false);
+
+  async function checkSession() {
+    const session = await getSession();
+    if (session) {
+      setSessionActive(true);
+    }
+    setSessionLoading(false);
+  }
+  checkSession();
+
+  if (sessionLoading) {
+    return null; // Ou algum componente de carregamento enquanto verifica a sessão
+  }
+
   return (
     <>
-      <Link href={'/'} className="flex items-center">
+      <Link href={'/'} passHref>
         <ButtonLink isMobile={isMobile}>Universidades</ButtonLink>
       </Link>
-      <Link href={'/avaliar'} className="flex items-center">
-        <ButtonLink isMobile={isMobile}>Avaliar</ButtonLink>
-      </Link>
+      {sessionActive && (
+        <Link href={'/avaliar'} passHref>
+          <ButtonLink isMobile={isMobile}>Avaliar</ButtonLink>
+        </Link>
+      )}
     </>
-  )
+  );
 }
