c449422c072a2620708bf7519cd9c6df2945d640
gabriel-ac6 <95120872+gabriel-ac6@users.noreply.github.com>
2024-06-27 14:24:48 -0300

    Update user-form.tsx

--- src/app/perfil/components/user-form.tsx
+++ src/app/perfil/components/user-form.tsx
@@ -1,7 +1,9 @@
 'use client'
 
 import { z } from 'zod'
-
+import { useQuery } from '@tanstack/react-query'
+import { serviceObterDadosUsuario } from '../../../services/auth/obter_dados'
+import { UserData } from '../../../services/auth/user_data.interface'
 import { Button } from '@/components/button'
 import { Flex } from '@/components/flex'
 import { Form } from '@/components/form/form'
@@ -9,9 +11,23 @@ import { Input } from '@/components/form/input'
 
 const schema = z.object({
   name: z.string(),
+  dob: z.string().optional(),
+  email: z.string().email(),
+  cpf: z.string().optional(),
+  password: z.string().optional(),
+  password_confirmation: z.string().optional(),
 })
 
 export function UserForm() {
+  const { data: userData, isLoading } = useQuery<UserData>({
+    queryKey: ['user-data'],
+    queryFn: serviceObterDadosUsuario,
+  })
+
+  if (isLoading) {
+    return <div>Carregando...</div>
+  }
+
   return (
     <Form
       schema={schema}
@@ -23,41 +39,41 @@ export function UserForm() {
 
         <Flex className="gap-4">
           <Flex className="gap-4 flex-col xl:flex-row">
-            <Input id="name" placeholder="Insira seu nome completo" type="text">
+            <Input
+              disabled
+              id="name"
+              placeholder="Insira seu nome completo"
+              type="text"
+              defaultValue={userData?.name || ''}
+            >
               Nome Completo
             </Input>
-            <Input id="dob" type="date" placeholder="dd/mm/yyyy">
+            <Input disabled id="dob" type="date" placeholder="dd/mm/yyyy" defaultValue={userData?.dob || ''}>
+              
               Data de Nascimento
             </Input>
           </Flex>
-          <Input id="email" type="email" placeholder="Insira seu email">
+          <Input
+            disabled
+            id="email"
+            type="email"
+            placeholder="Insira seu email"
+            defaultValue={userData?.email || ''}
+          >
             E-mail
           </Input>
-          <Input disabled id="cpf" type="text" placeholder="123.456.789-12">
+          <Input
+            disabled
+            id="cpf"
+            type="text"
+            placeholder="123.456.789-12"
+            defaultValue={userData?.id ? String(userData.cpf) : ''}
+          >
             CPF
           </Input>
         </Flex>
 
-        <Flex className="gap-4">
-          <h4>Senha</h4>
-          <Flex className="gap-4 flex-col xl:flex-row">
-            <Input id="password" type="password" placeholder="Insira sua senha">
-              Senha
-            </Input>
-            <Input
-              id="password_confirmation"
-              type="password"
-              placeholder="Confirme sua senha"
-            >
-              Confirmação de Senha
-            </Input>
-          </Flex>
-        </Flex>
       </Flex>
-
-      <Button type="submit" className="w-full lg:w-fit self-end">
-        Salvar
-      </Button>
     </Form>
   )
 }
